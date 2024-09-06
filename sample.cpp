86ba2709b1161eb69888de9bca56cfdb5ffc8891
jewel-106 <mjrcse106@gmail.com>
2024-09-07 01:23:11 +0600

    Commit first

---
+++ sample.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define Fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define Fread freopen("input.txt", "r", stdin);
+#define Fout freopen("output.txt", "w", stdout);
+using namespace std;
+
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define pb push_back
+#define ff first
+#define ss second
+#define YES cout <<"YES\n"
+#define NO cout << "NO\n"
+#define nn '\n'
+#define LL_INF (1LL << 62)
+#define INF (1 << 30)
+#define SetBit(x, k) (x = (1LL << k))
+#define ClearBit(x, k) (x &= ~(1LL << k))
+#define CheckBit(x, k) ((x >> k) & 1)
+
+#define mod 998244353
+#define N 300005
+#define pi acos(-1.0)                                                
+
+
+
+void solve()
+{
+   
+}
+
+
+int main()
+{
+    Fast
+ #ifndef ONLINE_JUDGE
+     Fread
+     Fout
+ #endif
+    ll t=1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
